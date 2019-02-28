1b336a2c2ff436895b122261db1256787d191836
pokercc <pokercc@sina.com>
2019-02-28 22:43:52 +0800

    实现了基本功能

--- src/main/kotlin/pokercc/java/message/Message.kt
+++ src/main/kotlin/pokercc/java/message/Message.kt
@@ -2,6 +2,7 @@ package pokercc.java.message
 
 import org.omg.CORBA.Object
 import java.util.*
+import java.util.concurrent.locks.ReentrantLock
 
 class Message(
     var what: Int,
@@ -13,8 +14,11 @@ class Message(
 
 }
 
-open class Handler(val looper: Looper = Looper.myLooper()!!) {
+open class Handler(private val looper: Looper = Looper.myLooper()!!) {
     open fun handleMessage(message: Message) {
+        if (message.runnable != null) {
+            message.runnable!!.run()
+        }
 
     }
 
@@ -27,6 +31,10 @@ open class Handler(val looper: Looper = Looper.myLooper()!!) {
 
 class Looper private constructor() {
     var messageQueue = MessageQueue()
+
+    fun quit() {
+        messageQueue.quit()
+    }
 
     companion object {
         private val looperThreadLocal = ThreadLocal<Looper>()
@@ -57,15 +65,12 @@ class Looper private constructor() {
         fun loop() {
             val myLooper: Looper = myLooper() ?: throw IllegalStateException("looper not exists")
             while (true) {
-                val message = myLooper.messageQueue.next()
-                if (message != null) {
-                    message.target.handleMessage(message)
-                } else {
-                    Thread.yield()
-                }
+                val message = myLooper.messageQueue.next() ?: break
+                message.target.handleMessage(message)
 
             }
         }
+
 
         @JvmStatic
         fun prepareMainLooper() {
@@ -81,15 +86,45 @@ class Looper private constructor() {
 
 class MessageQueue {
     private val queue: Deque<Message> = LinkedList<Message>()
-
+    //    private val lock = ReentrantLock()
+    private var quit = false
+    private val lock = Object()
     fun enqueue(message: Message) {
+
+        val empty = queue.isEmpty()
         queue.add(message)
+        if (empty) {
+
+            synchronized(lock) {
+                println("有新消息，已经解锁")
+                lock.notifyAll()
+            }
+        }
+
+//        if (lock.isLocked) {
+//
+//            lock.unlock()
+//            println("有信消息，已经解锁")
+//        }
     }
 
     fun next(): Message? {
-        if (queue.size != 0) {
-            return queue.poll()
+        if (queue.isEmpty()) {
+            if (quit) {
+                println("已退出")
+                return null
+            }
+            println("消息队列为空，已经锁定")
+            synchronized(lock) {
+                lock.wait()
+            }
+//            lock.lock()
         }
-        return null
+        return queue.poll()
+
+    }
+
+    fun quit() {
+        quit = true
     }
 }
--- src/main/kotlin/pokercc/java/message/MessageDemo.kt
+++ src/main/kotlin/pokercc/java/message/MessageDemo.kt
@@ -3,24 +3,33 @@ package pokercc.java.message
 import java.util.*
 
 fun main(args: Array<String>) {
+    Looper.prepareMainLooper()
     val handler = Handler()
 
     Thread {
-        println("请输入内容")
+        println("请输入内容(输入q退出)")
         val scanner = Scanner(System.`in`)
-        while (scanner.hasNextLine()) {
+        var quit = false
+        while (scanner.hasNextLine() && !quit) {
             val line = scanner.nextLine()
-            val message = Message(0, runnable = Runnable {
-                print("${Thread.currentThread().name}  接收到:$line")
-            })
-            handler.send(message)
+            println("${Thread.currentThread().name}  输入的:$line")
+
+            handler.send(Message(0, runnable = Runnable {
+                println("${Thread.currentThread().name}  接收到:$line")
+            }))
+
+            when (line) {
+                "q" -> {
+                    Looper.mainLooper().quit()
+                    quit = true
+                }
+            }
         }
 
     }.also {
         it.name = "子线程"
         it.start()
     }
-    Looper.prepareMainLooper()
     Looper.loop()
 
 }
